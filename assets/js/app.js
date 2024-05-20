c4cd0858b586ca7141b529bc31efb7f103e5e888
alarcon steven <devalarcon.23@gmail.com>
2024-05-20 12:03:29 -0500

    cells added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     let currentPlayer = 'X';
-
     const cells = document.querySelectorAll('.cell');
+    const message = document.querySelector('#message');
+    const btnNew = document.querySelector('#btnNew');
+
     cells.forEach(cell => cell.addEventListener('click', handleClick));
 
     function handleClick() {
@@ -9,22 +11,23 @@ document.addEventListener('DOMContentLoaded', function() {
         this.textContent = currentPlayer;
 
         if (checkWinner(currentPlayer)) {
-          alert(currentPlayer + ' gana!');
-          resetBoard();
+          message.textContent = currentPlayer + ' gana!';
+        
         } else if (checkDraw()) {
-          alert('Empate!');
-          resetBoard();
+          message.textContent = 'Empate!';
+        
         } else {
           currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+          message.textContent = "Turno de " + currentPlayer;
         }
       }
     }
 
     function checkWinner(player) {
       const winConditions = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-        [0, 4, 8], [2, 4, 6]             // diagonals
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // filas
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columnas
+        [0, 4, 8], [2, 4, 6]             // diagonales
       ];
 
       return winConditions.some(condition =>
@@ -36,8 +39,11 @@ document.addEventListener('DOMContentLoaded', function() {
       return Array.from(cells).every(cell => cell.textContent !== '-');
     }
 
-    function resetBoard() {
+    
+    btnNew.addEventListener('click', function(){
       cells.forEach(cell => cell.textContent = '-');
       currentPlayer = 'X';
-    }
+      message.textContent = '';
+    })
+
   });
